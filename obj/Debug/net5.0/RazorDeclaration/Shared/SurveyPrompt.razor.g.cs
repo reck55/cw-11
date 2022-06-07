3b77a98f66c673ad9e48cdc5f729bfe735de4d12
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 19:48:34 +0200

    css

--- obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,77 +13,77 @@ namespace blazor_24c.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 1 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 2 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 3 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 4 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 5 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 6 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 7 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 8 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 9 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using blazor_24c;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 10 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using blazor_24c.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Uczelnia\Desktop\blazor-24c\_Imports.razor"
+#line 11 "C:\Users\stefa\Desktop\blazor-24c\_Imports.razor"
 using blazor_24c.Data;
 
 #line default
@@ -97,7 +97,7 @@ using blazor_24c.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Uczelnia\Desktop\blazor-24c\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\stefa\Desktop\blazor-24c\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
